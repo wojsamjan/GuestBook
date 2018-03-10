625e979b2d5519750b9cb995156d5f4029073493
Wojciech Januszek <wojsamjan@gmail.com>
2018-03-10 21:26:22 +0100

    add Controller tests

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,14 +1,24 @@
 require 'test_helper'
 
-class PostsControllerTest < ActionDispatch::IntegrationTest
+class PostsControllerTest < ActionController::TestCase
   test "should get index" do
-    get posts_index_url
+    get :index
     assert_response :success
+    assert_not_nil assigns(:posts)
   end
 
   test "should get new" do
-    get posts_new_url
+    get :new
     assert_response :success
+    assert_not_nil assigns(:post)
+  end
+
+  test "should create a post" do
+    posts_before = Post.count
+    post :create, params: { post: { author: "Autor", body: "Wpis" } }
+    posts_after = Post.count
+    assert_equal 1, posts_after - posts_before
+    assert_redirected_to posts_path
   end
 
 end
