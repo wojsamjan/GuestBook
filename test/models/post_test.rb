58be063e0afa03abcb24a47aebcb3e4320a8e7e2
Wojciech Januszek <wojsamjan@gmail.com>
2018-03-10 20:06:45 +0100

    add model test

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "formats author" do
+    p = Post.create(author: "john locke", body: "Hello!")
+    assert_equal "John Locke", p.author
+  end
 end
