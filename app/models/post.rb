1c0e8c7036f55198e0143755f24e9ae2c760c74d
Wojciech Januszek <wojsamjan@gmail.com>
2018-03-11 19:11:48 +0100

    add after_create callback to Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,14 @@
 class Post < ApplicationRecord
   before_create :titleize_author
+  after_create :notify_admin
 
   private
 
   def titleize_author
     self.author = self.author.to_s.titleize
+  end
+
+  def notify_admin
+    PostsMailer.new_post_notification(self).deliver
   end
 end
