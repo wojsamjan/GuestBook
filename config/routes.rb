600cb2256a249b9d547394712d7cffeeb01679e3
Wojciech Januszek <wojsamjan@gmail.com>
2018-03-11 22:35:27 +0100

    add locales to config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   root 'posts#index'
   resources :posts do
-    collection do 
+    collection do
       get :surprise
     end
   end
+
+  get 'locale', to: 'locales#save_locale', as: :set_locale
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
