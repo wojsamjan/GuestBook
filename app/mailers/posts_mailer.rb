a2bcac5536d0a363afdea66e69ae0f933fd4a6e8
Wojciech Januszek <wojsamjan@gmail.com>
2018-03-11 19:11:03 +0100

    generate PostsMailer

---
+++ app/mailers/posts_mailer.rb
@@ -0,0 +1,13 @@
+class PostsMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.posts_mailer.new_post_notification.subject
+  #
+  def new_post_notification(post)
+    @post = post
+
+    mail to: "to@example.org", subject: "Wow, nowy post!"
+  end
+end
