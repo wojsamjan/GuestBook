1e5d714d05fd1e12939463c2e303681e40c50a09
Wojciech Januszek <wojsamjan@gmail.com>
2018-03-11 18:20:42 +0100

    add 'creates a post' integration test

--- test/integration/guest_book_test.rb
+++ test/integration/guest_book_test.rb
@@ -4,5 +4,14 @@ class GuestBookTest < ActionDispatch::IntegrationTest
   end
 
   test "creates a post" do
-    
+    visit posts_path
+    click_link "Dodaj własny wpis już teraz."
+    fill_in "Autor", with: "Testowy Autor"
+    fill_in "Treść", with: "Testowa Treść"
+    click_button "Dodaj wpis"
+
+    assert page.has_content?("Dziękujemy za wpis!"), "expected a flash message"
+    assert page.has_content?("Testowy Autor")
+    assert page.has_content?("Testowa Treść")
   end
+end
